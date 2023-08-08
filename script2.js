0f05e8a4ca3850d5383ab4da7b090def236d0829
Aji <ajibola128@icloud.com>
2023-08-08 17:53:22 +0100

    code cleanup

--- script2.js
+++ script2.js
@@ -1,29 +1,29 @@
 
-const popup = document.getElementById("popup-background");
-const button = document.getElementById("button");
-const button2 = document.getElementById("button2");
-const body = document.getElementById("body");
-let header = document.getElementsByClassName("header")[0];
+// const popup = document.getElementById("popup-background");
+// const button = document.getElementById("button");
+// const button2 = document.getElementById("button2");
+// const body = document.getElementById("body");
+// let header = document.getElementsByClassName("header")[0];
 
-const showPopup = () => {
-    popup.style.display = 'flex';
+// const showPopup = () => {
+//     popup.style.display = 'flex';
 
-}
+// }
 
-function hidePopUP() {
-    popup.style.display = "none";
-}
+// function hidePopUP() {
+//     popup.style.display = "none";
+// }
 
-function myAlert() {
-   hidePopUP();
-}
+// function myAlert() {
+//    hidePopUP();
+// }
 
 
 
-/* NIGHT MODE*/
-const onClick = () => {
-    showPopup()
-}
+// /* NIGHT MODE*/
+// const onClick = () => {
+//     showPopup()
+// }
 
 
 
